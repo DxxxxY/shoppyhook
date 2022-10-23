af52d156c2022ae066c5647db727172ea2d3319b
dxxxxy <introsdream@gmail.com>
2022-10-22 21:16:47 -0400

    issue was bodyparser on api

--- index.js
+++ index.js
@@ -2,13 +2,9 @@ const crypto = require("crypto")
 
 module.exports = options => {
     return (req, res, next) => {
-        console.log("log")
-
         req.rawBody = ""
 
         req.on("data", (chunk) => {
-            console.log("chunk")
-
             req.rawBody += chunk
         })
 
@@ -18,20 +14,18 @@ module.exports = options => {
 
                 console.log(req.rawBody)
 
-                if (!req.headers["x-shoppy-signature"]) return
+                if (!req.headers["x-shoppy-signature"]) return res.status(400).send("Missing signature header")
 
                 const hmac = crypto.createHmac("sha512", options.secret)
                 const signed = hmac.update(Buffer.from(req.rawBody, "utf-8")).digest("hex")
 
-                if (signed !== req.headers["x-shoppy-signature"]) return
+                if (signed !== req.headers["x-shoppy-signature"]) return res.status(401).send("Invalid signature")
 
                 next()
             } catch (err) {
                 console.log("Error parsing body")
-                next()
+                res.status(400).send("Error parsing body")
             }
         })
-
-        res.send("ok")
     }
 }
