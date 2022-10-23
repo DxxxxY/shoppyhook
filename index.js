0e813840ee0c6289f16c63b0b6dc6cdbb0f01332
dxxxxy <introsdream@gmail.com>
2022-10-22 20:21:31 -0400

    test req.body

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const crypto = require("crypto")
 
 module.exports = options => {
     return (req, res, next) => {
+        console.log(req.body)
+
         if (!req.headers["x-shoppy-signature"]) return res.status(400).send("Missing signature header")
 
         const hmac = crypto.createHmac("sha512", options.secret)
-        const signed = hmac.update(Buffer.from(req.rawBody, "utf-8")).digest("hex")
+        const signed = hmac.update(Buffer.from(req.body, "utf-8")).digest("hex")
 
         if (signed !== req.headers["x-shoppy-signature"]) return res.status(401).send("Invalid signature")
 
